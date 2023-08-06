9e13643c705e32df8e8524457ecb7496f3b2475f
Ivan Artsiushyn <ivanart555@gmail.com>
2023-08-06 17:35:03 +0300

    Fix for 12 task

--- lesson12/src/main/resources/TABLES_CHANGES.sql
+++ lesson12/src/main/resources/TABLES_CHANGES.sql
@@ -1,1 +1,8 @@
-select * from student;
+ALTER TABLE Mark DROP CONSTRAINT mark_student_id_fkey;
+ALTER TABLE Mark ADD CONSTRAINT mark_student_id_fkey FOREIGN KEY (student_id) REFERENCES Student(id) ON DELETE CASCADE;
+
+ALTER TABLE Payment DROP CONSTRAINT payment_student_id_fkey;
+ALTER TABLE Payment ADD CONSTRAINT payment_student_id_fkey FOREIGN KEY (student_id) REFERENCES Student(id) ON DELETE CASCADE;
+
+ALTER TABLE Payment DROP CONSTRAINT payment_type_id_fkey;
+ALTER TABLE Payment ADD CONSTRAINT payment_type_id_fkey FOREIGN KEY (type_id) REFERENCES PaymentType(id) ON DELETE CASCADE;
